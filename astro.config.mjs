a0544b0962a3bbf001086d0b4e3a4acbe2ac093a
Lucas Pintos <lucaspintos909@gmail.com>
2026-02-27 02:04:09 -0300

    chore: Update Astro configuration site URL.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://lucaspintos.vercel.app',
+  site: 'https://www.lucaspintos.link',
   output: 'static',
   build: {
     inlineStylesheets: 'always',
